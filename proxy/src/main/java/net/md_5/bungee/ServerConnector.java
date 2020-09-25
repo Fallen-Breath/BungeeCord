d1704157e07f463ec61a798c4a42f75a136ecc49
Fallen_Breath <yqx791125833@gmail.com>
2020-09-25 21:48:20 +0800

    hacky fix for no client reloading when switching server from 1.16 to 1.15

--- proxy/src/main/java/net/md_5/bungee/ServerConnector.java
+++ proxy/src/main/java/net/md_5/bungee/ServerConnector.java
@@ -278,7 +278,7 @@ public class ServerConnector extends PacketHandler
             }
 
             user.setDimensionChange( true );
-            if ( login.getDimension() == user.getDimension() )
+            if ( login.getDimension() == user.getDimension() || true )  // hacky fix for no client reloading when switching server from 1.16 to 1.15
             {
                 user.unsafe().sendPacket( new Respawn( (Integer) login.getDimension() >= 0 ? -1 : 0, login.getWorldName(), login.getSeed(), login.getDifficulty(), login.getGameMode(), login.getPreviousGameMode(), login.getLevelType(), login.isDebug(), login.isFlat(), false ) );
             }
